60a4a138b09c65593ac456072cc8495bf91ac959
Ian Snider <irsnider13@gmail.com>
2026-01-13 15:32:37 -0500

    Added success message code

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const form = document.getElementById("form");
+const dismiss = document.getElementById("dismiss");
 
 function handleSubmit(e){
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.target));
 
-    console.log(data);
+    console.log(data.email);
+
+    if(data.email.includes('@')){
+        document.getElementById("success").style.display = "block";
+    }
 }
 
+function onDismiss(e){
+    document.getElementById('success').style.display = "none";
+}
+
+dismiss.addEventListener('click', onDismiss);
 form.addEventListener('submit', handleSubmit);
