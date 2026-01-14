9dfdfbd04eb789fdbea5559a5942032225620751
Ian Snider <irsnider13@gmail.com>
2026-01-14 14:37:23 -0500

    Hidden form on success

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ function handleSubmit(e){
 
     if(data.email.includes('@')){
         document.getElementById("success").style.display = "block";
+        form.style.display = "none";
     }
 }
 
 function onDismiss(e){
     document.getElementById('success').style.display = "none";
+    form.style.display = "block";
 }
 
 dismiss.addEventListener('click', onDismiss);
