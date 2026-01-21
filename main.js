44f4ddeb93cd8bf60fb112b18913eaa6b88d7f90
Ian Snider <irsnider13@gmail.com>
2026-01-21 10:30:31 -0500

    Added email validation

--- main.js
+++ main.js
@@ -8,8 +8,15 @@ function handleSubmit(e){
 
     console.log(data.email);
 
-    if(data.email.includes('@')){
+    if(data.email === "" || !data.email.includes('@')){
+        document.getElementById('error').style.display = "inline";
+        document.getElementById('email').style.backgroundColor = "hsl(1, 100%, 90%)";
+        document.getElementById('email').style.color = "hsl(4, 100%, 67%)";
+        document.getElementById('email').style.borderColor = "hsl(4, 100%, 67%)";
+    }
+    else if(data.email.includes('@')){
         document.getElementById("success").style.display = "block";
+        document.getElementById('error').style.display = "none";
         form.style.display = "none";
     }
 }
