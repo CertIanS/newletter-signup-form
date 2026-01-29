9266c3848451448252523ba303e6fe6d75804210
Ian Snider <irsnider13@gmail.com>
2026-01-29 10:10:37 -0500

    Added email validation while typing

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 const form = document.getElementById("form");
 const dismiss = document.getElementById("dismiss");
+const emailInput = document.getElementById('email');
 
 function handleSubmit(e){
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.target));
 
-    console.log(data.email);
-
     if(data.email === "" || !data.email.includes('@')){
         document.getElementById('error').style.display = "inline";
-        document.getElementById('email').style.backgroundColor = "hsl(1, 100%, 90%)";
-        document.getElementById('email').style.color = "hsl(4, 100%, 67%)";
-        document.getElementById('email').style.borderColor = "hsl(4, 100%, 67%)";
+        emailInput.style.backgroundColor = "hsl(1, 100%, 90%)";
+        emailInput.style.color = "hsl(4, 100%, 67%)";
+        emailInput.style.borderColor = "hsl(4, 100%, 67%)";
     }
     else if(data.email.includes('@')){
         document.getElementById('error').style.display = "none";
-        document.getElementById('email').style.backgroundColor = "white";
-        document.getElementById('email').style.color = "black";
-        document.getElementById('email').style.borderColor = "hsl(0, 0%,58%)";
+        emailInput.style.backgroundColor = "white";
+        emailInput.style.color = "black";
+        emailInput.style.borderColor = "hsl(0, 0%, 58%)";
         document.getElementById("card").style.display = "none";
         document.getElementById("success").style.display = "block";
         document.getElementById("emailText").textContent = data.email;
@@ -28,6 +27,15 @@ function handleSubmit(e){
 function onDismiss(e){
     document.getElementById('success').style.display = "none";
     document.getElementById("card").style.display = "flex";
+}
+
+function handleText(email){
+    if(email.includes('@')){
+        document.getElementById('error').style.display = "none";
+        emailInput.style.backgroundColor = "white";
+        emailInput.style.color = "black";
+        emailInput.style.borderColor = "hsl(0, 0%,58%)";
+    }
 }
 
 dismiss.addEventListener('click', onDismiss);
